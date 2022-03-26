38dc0a3392c3eb62f9cd60de79f0c1e245ff3b83
Ferney Medina <ferney@docketwise.com>
2022-03-26 11:47:42 -0500

    add category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_26_162615) do
+ActiveRecord::Schema.define(version: 2022_03_26_164708) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 2022_03_26_162615) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["currency_id"], name: "index_acounts_on_currency_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "label"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "currencies", force: :cascade do |t|
