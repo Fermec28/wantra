ba65e83846e868ad7c2cb7f6e078457790d22829
Ferney Medina <ferney@docketwise.com>
2022-04-14 12:08:59 -0500

    add acount Page

---
+++ app/javascript/packs/Pages/AccountPage/AccountPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import SideBar from "../../Components/Sidebar/SideBar";
+import AccountDetail from "../AccountDetail/AccountDetail";
+import { Routes, Route } from "react-router-dom";
+
+const AccountPage = () => {
+    return (
+        <div class="flex-grow flex overflow-x-hidden">
+          <SideBar />
+          <Routes>
+            <Route path="/:accountId"  element={<AccountDetail />} />
+          </Routes>
+          
+        </div>
+    )
+}
+
+export default AccountPage;
